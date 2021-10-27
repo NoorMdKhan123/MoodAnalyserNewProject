c9c2319f323abce2d23edf982943c7869f9b4e1e
NoorMdKhan123 <noormd6400@gmail.com>
2021-10-27 23:54:58 +0530

    Welcome to Mood Analyser Practice Program

---
+++ MoodAnalyserPojectProgram/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MoodAnalyserPojectProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mood Analyser Practice Problem");
+        }
+    }
+}
